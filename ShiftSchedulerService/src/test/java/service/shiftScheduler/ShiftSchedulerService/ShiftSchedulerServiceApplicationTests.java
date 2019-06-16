1687aec215281f878d44a93699eceb2072101da5
Toma1997 <toma.joksimovic@gmail.com>
2019-06-16 16:42:50 +0200

    Added structure for both web services

---
+++ ShiftSchedulerService/src/test/java/service/shiftScheduler/ShiftSchedulerService/ShiftSchedulerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package service.shiftScheduler.ShiftSchedulerService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShiftSchedulerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
